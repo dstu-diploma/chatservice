d126b2ec2697e859f0e2f28b55575d4c27913eec
maladesov <m.zaharenkov@mail.ru>
2025-05-18 01:24:19 +0300

    ✨ Названия хакатонов и использование dto_utils

--- app/services/requests/service.py
+++ app/services/requests/service.py
@@ -5,7 +5,7 @@ from app.models.chat import RequestModel, MessageModel
 from app.ports.userservice import IUserServicePort
 from tortoise.transactions import in_transaction
 from .dto import MessageDto, RequestDto
-from collections import defaultdict
+from app.util import dto_utils
 
 from .exceptions import (
     CantSendMessagesRequestIsClosedException,
@@ -22,68 +22,48 @@ class RequestService(IRequestService):
         self.user_service = user_service
         self.hackathon_service = hackathon_service
 
-    def _get_request_user_ids(
-        self, requests: list[RequestDto]
-    ) -> frozenset[int]:
-        return frozenset(
-            request.author_user_id for request in requests
-        ) | frozenset(
-            request.closed_by_user_id
-            for request in requests
-            if request.closed_by_user_id
-        )
-
-    def _get_message_user_ids(
-        self, messages: list[MessageDto]
-    ) -> frozenset[int]:
-        return frozenset(message.user_id for message in messages)
-
-    def _inject_request_names(
-        self, dtos: list[RequestDto], names: defaultdict[int, str | None]
-    ):
-        for dto in dtos:
-            dto.author_name = names[dto.author_user_id]
-
-            if dto.closed_by_user_id:
-                dto.closed_by_name = names[dto.closed_by_user_id]
-
-    def _inject_message_names(
-        self, dtos: list[MessageDto], names: defaultdict[int, str | None]
-    ):
-        for dto in dtos:
-            dto.user_name = names[dto.user_id]
-
-    async def _request_models_to_dtos(
-        self, instances: list[RequestModel]
-    ) -> list[RequestDto]:
-        dtos = [RequestDto.from_tortoise(request) for request in instances]
-        names = self.user_service.get_name_map(
+    async def _inject_names(self, dtos: list[RequestDto]) -> list[RequestDto]:
+        user_names = self.user_service.get_name_map(
             await self.user_service.try_get_user_info_many(
-                self._get_request_user_ids(dtos)
-            )
-        )
-        self._inject_request_names(dtos, names)
-        return dtos
-
-    async def _message_models_to_dtos(
-        self, instances: list[MessageModel]
-    ) -> list[MessageDto]:
-        dtos = [MessageDto.from_tortoise(message) for message in instances]
-        names = self.user_service.get_name_map(
-            await self.user_service.try_get_user_info_many(
-                self._get_message_user_ids(dtos)
+                dto_utils.export_int_fields(
+                    dtos, "author_user_id", "closed_by_user_id"
+                )
             )
         )
 
-        self._inject_message_names(dtos, names)
+        hackathon_names = self.hackathon_service.get_name_map(
+            await self.hackathon_service.try_get_hackathon_data_many(
+                dto_utils.export_int_fields(dtos, "hackathon_id")
+            )
+        )
+
+        dtos = dto_utils.inject_mapping(
+            dtos, user_names, "_user_id", "_name", strict=False
+        )
+        dtos = dto_utils.inject_mapping(
+            dtos,
+            hackathon_names,
+            "hackathon_id",
+            "hackathon_name",
+            strict=True,
+        )
+
         return dtos
 
     async def get_all_requests(self) -> list[RequestDto]:
-        return await self._request_models_to_dtos(await RequestModel.all())
+        dtos = [
+            RequestDto.from_tortoise(request)
+            for request in await RequestModel.all()
+        ]
+
+        return await self._inject_names(dtos)
 
     async def get_requests_by_user(self, user_id: int) -> list[RequestDto]:
-        return await self._request_models_to_dtos(
-            await RequestModel.filter(author_user_id=user_id)
+        return await self._inject_names(
+            [
+                RequestDto.from_tortoise(request)
+                for request in await RequestModel.filter(author_user_id=user_id)
+            ]
         )
 
     async def _get_request(self, request_id: int) -> RequestModel:
@@ -94,16 +74,27 @@ class RequestService(IRequestService):
         return request
 
     async def get_request(self, request_id: int) -> RequestDto:
-        dtos = await self._request_models_to_dtos(
-            [await self._get_request(request_id)]
+        dtos = await self._inject_names(
+            [RequestDto.from_tortoise(await self._get_request(request_id))]
         )
 
         return dtos[0]
 
     async def get_request_history(self, request_id: int) -> list[MessageDto]:
         await self._get_request(request_id)
-        return await self._message_models_to_dtos(
-            await MessageModel.filter(request_id=request_id)
+        dtos = [
+            MessageDto.from_tortoise(message)
+            for message in await MessageModel.filter(request_id=request_id)
+        ]
+
+        user_names = self.user_service.get_name_map(
+            await self.user_service.try_get_user_info_many(
+                dto_utils.export_int_fields(dtos, "user_id")
+            )
+        )
+
+        return dto_utils.inject_mapping(
+            dtos, user_names, "user_id", "user_name", strict=True
         )
 
     async def is_request_open(self, request_id: int) -> bool:
@@ -148,7 +139,7 @@ class RequestService(IRequestService):
 
         user_info = await self.user_service.try_get_user_info(author_user_id)
         if user_info:
-            request_dto.author_name = self.user_service.format_name(user_info)
+            request_dto.author_name = user_info.formatted_name
 
         Emitter.emit(Events.RequestOpened, request_dto)
         return request_dto
@@ -166,7 +157,7 @@ class RequestService(IRequestService):
         message_dto = MessageDto.from_tortoise(message_obj)
         user_info = await self.user_service.try_get_user_info(user_id)
         if user_info:
-            message_dto.user_name = self.user_service.format_name(user_info)
+            message_dto.user_name = user_info.formatted_name
 
         Emitter.emit(Events.Message, message_dto)
         return message_dto
